890b35f0ee74519bfd325a504f469900f1aba6b3
Amanda Nikrant <ajnikrant@gmail.com>
2021-01-21 15:43:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-
+  new_array = ["wow", "arrays!"]
+  new_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
