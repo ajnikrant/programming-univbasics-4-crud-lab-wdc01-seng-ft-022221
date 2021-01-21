1959ffc02844b22ca0ae7972dc64f74a3ff4342f
Amanda Nikrant <ajnikrant@gmail.com>
2021-01-21 15:41:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,12 +8,12 @@ end
 
 def add_element_to_end_of_array(array, element)
 new_array = []
-new_array << "wow"
+new_array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
   new_array = []
-  new_array.unshift "arrays!"
+  new_array.unshift "wow"
 end
 
 def remove_element_from_end_of_array(array)
