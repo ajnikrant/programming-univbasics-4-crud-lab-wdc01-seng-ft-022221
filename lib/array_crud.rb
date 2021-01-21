88ac0287fda1496abb458d9057877c7ffa23e5cd
Amanda Nikrant <ajnikrant@gmail.com>
2021-01-21 15:44:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  new_array = ["I", "am", "ready"]
+  puts new_array[2]
 end
 
 def retrieve_first_element_from_array(array)
